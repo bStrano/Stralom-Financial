aed5b601e3af0101878a593d94db2d3d1cb60195
Bruno Strano <bruno_strano@hotmail.com>
2021-02-16 21:20:12 -0300

    Codepush

--- android/app/src/main/java/com/stralom/financial/MainApplication.java
+++ android/app/src/main/java/com/stralom/financial/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -36,6 +37,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+          @Override
+          protected String getJSBundleFile() {
+              return CodePush.getJSBundleFile();
+          }
       };
 
   @Override
